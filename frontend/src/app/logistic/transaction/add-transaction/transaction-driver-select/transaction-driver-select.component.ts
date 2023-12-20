b9e462411b77fb651c62cd26d52075bbda94162f
PhucnnPC <phucnn.pc@gmail.com>
2023-12-20 13:09:57 +0700

    update

--- frontend/src/app/logistic/transaction/add-transaction/transaction-driver-select/transaction-driver-select.component.ts
+++ frontend/src/app/logistic/transaction/add-transaction/transaction-driver-select/transaction-driver-select.component.ts
@@ -12,15 +12,10 @@ import { DatePipe } from '@angular/common';
 	styleUrls: ['./transaction-driver-select.component.scss']
 })
 export class TransactionDriverSelectComponent implements OnInit {
-	control = new FormControl();
+	@Input() control = new FormControl();
 	@Output() change = new EventEmitter();
 	drivers: Driver[];
-  driverBrithdate: string = '';
-  drivername: string = '';
-  phoneNo: string = '';
-  identityCardNo: string = '';
-  homeTown: string = '';
-	driverId: number;
+	driverSelected: Driver;
 
 	constructor(
 		public driverService: DriverService,
@@ -32,29 +27,27 @@ export class TransactionDriverSelectComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-    
-    this.control.valueChanges.subscribe(selectedId  => {
-      const selectedDriver = this.driverService.list.find(driver => driver.id === selectedId);
-      this.onSelectChange(selectedDriver);
-    });
+		this.control.valueChanges.subscribe(selectedId  => {
+			const selectedDriver = this.driverService.list.find(driver => driver.id === selectedId);
+			this.onSelectChange(selectedDriver);
+		});
 	}
 
-  initForm() {
+  	initForm() {
 
 	}
 
 	compareFn = (o1: any, o2: any): boolean => (o1 && o2 ? o1.value === o2.value : o1 === o2);
 
-	onSelectChange(drivers): void {
-    var pattern = /(\d{4})(\d{2})(\d{2})/;
-    this.driverBrithdate = this.datePipe.transform(new Date(drivers.dateOfBirth.replace(pattern, '$1-$2-$3')), 'dd/MM/yyyy');
-    this.phoneNo = drivers.phoneNo;
-    this.identityCardNo = drivers.identityCardNo;
-    this.homeTown = drivers.homeTown;
-    this.driverId = drivers.id;
-    this.drivername = drivers.fullName;
-    // Handle the change as needed
-  }
+	onSelectChange(selectedDriver): void {
+		this.driverSelected = selectedDriver;
+		console.log(this.driverSelected);
+	}
+
+	formatHumanDate(dateString) : string {
+		var pattern = /(\d{4})(\d{2})(\d{2})/;
+		return this.datePipe.transform(new Date(dateString.replace(pattern, '$1-$2-$3')), 'dd/MM/yyyy');
+	}
 
 	openEditModal() {
 		this._modalService.create({
--- frontend/src/app/logistic/transaction/add-transaction/transaction-vehicle-select/transaction-vehicle-select.component.ts
+++ frontend/src/app/logistic/transaction/add-transaction/transaction-vehicle-select/transaction-vehicle-select.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { Vehicle } from 'src/app/__models/vehicle';
+import { VehicleService } from 'src/app/__services/vehicle.service';
+import { AddVehicleModalComponent } from 'src/app/logistic/vehicle/add-vehicle-modal/add-vehicle-modal.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-transaction-vehicle-select',
-  templateUrl: './transaction-vehicle-select.component.html',
-  styleUrls: ['./transaction-vehicle-select.component.scss']
+	selector: 'app-transaction-vehicle-select',
+	templateUrl: './transaction-vehicle-select.component.html',
+	styleUrls: ['./transaction-vehicle-select.component.scss']
 })
 export class TransactionVehicleSelectComponent implements OnInit {
+	@Input() control = new FormControl();
+	@Output() change = new EventEmitter();
+	vehicles: Vehicle[];
+	vehicleSelected: Vehicle;
 
-  constructor() { }
+	constructor(
+		public vehicleService: VehicleService,
+		private _modalService: NzModalService,
+    private datePipe: DatePipe
+	) {
+		if (!vehicleService.list)
+			vehicleService.refreshList();
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.control.valueChanges.subscribe(selectedId  => {
+			const selectedVehicle = this.vehicleService.list.find(vehicle => vehicle.id === selectedId);
+			this.onSelectChange(selectedVehicle);
+		});
+	}
 
+  	initForm() {
+
+	}
+
+	compareFn = (o1: any, o2: any): boolean => (o1 && o2 ? o1.value === o2.value : o1 === o2);
+
+	onSelectChange(selectedVehicle): void {
+		this.vehicleSelected = selectedVehicle;
+		console.log(this.vehicleSelected);
+	}
+
+	openEditModal() {
+		this._modalService.create({
+			nzContent: AddVehicleModalComponent,
+			nzClosable: false,
+			nzFooter: null,
+			nzWidth: 700,
+			nzComponentParams: {
+				title: 'Thêm mới Phương tiện',
+				model: null
+			}
+		});
+	}
 }
