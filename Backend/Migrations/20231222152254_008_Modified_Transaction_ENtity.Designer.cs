df393e0169b8d73f98da57992f730e4121fa4258
mateo nguyen <work.toannv@gmail.com>
2023-12-22 22:23:43 +0700

    update transaction entity

--- Backend/Migrations/20231222152254_008_Modified_Transaction_ENtity.Designer.cs
+++ Backend/Migrations/20231222152254_008_Modified_Transaction_ENtity.Designer.cs
@@ -4,6 +4,7 @@ using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(WakDbContext))]
-    partial class WakDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231222152254_008_Modified_Transaction_ENtity")]
+    partial class _008_Modified_Transaction_ENtity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Migrations/20231222152254_008_Modified_Transaction_ENtity.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class _008_Modified_Transaction_ENtity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveredPlace",
+                table: "TransactionDetails");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShippingRouteId",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 7);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeliveredPlaceId",
+                table: "TransactionDetails",
+                type: "int",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ShippingRouteId",
+                table: "Transactions",
+                column: "ShippingRouteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionDetails_DeliveredPlaceId",
+                table: "TransactionDetails",
+                column: "DeliveredPlaceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TransactionDetails_Locations_DeliveredPlaceId",
+                table: "TransactionDetails",
+                column: "DeliveredPlaceId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_ShippingRoutes_ShippingRouteId",
+                table: "Transactions",
+                column: "ShippingRouteId",
+                principalTable: "ShippingRoutes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TransactionDetails_Locations_DeliveredPlaceId",
+                table: "TransactionDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_ShippingRoutes_ShippingRouteId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_ShippingRouteId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TransactionDetails_DeliveredPlaceId",
+                table: "TransactionDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingRouteId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveredPlaceId",
+                table: "TransactionDetails");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveredPlace",
+                table: "TransactionDetails",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true);
+        }
+    }
+}
