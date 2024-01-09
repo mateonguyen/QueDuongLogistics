7987a2a59339483eb1da4ee86771849df05c42e1
Nguyen Ngoc Phuc <nnp1.happy@gmail.com>
2024-01-09 22:42:46 +0700

    Upload anh vendor

--- frontend/src/app/logistic/vendor/add-vendor-modal/add-vendor-modal.component.ts
+++ frontend/src/app/logistic/vendor/add-vendor-modal/add-vendor-modal.component.ts
@@ -1,26 +1,33 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Vendor } from 'src/app/__models/vendor';
-import { NzModalRef } from 'ng-zorro-antd/modal';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
 import { VendorService } from 'src/app/__services/vendor.service';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { LogoCropperComponent } from '../../__components/logo-cropper/logo-cropper.component';
+import { base64ToFile } from 'ngx-image-cropper';
 
 @Component({
 	selector: 'app-add-vendor-modal',
 	templateUrl: 'add-vendor-modal.component.html',
-	styleUrls: ['add-vendor-modal.component.scss']
+	styleUrls: ['add-vendor-modal.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class AddVendorModalComponent implements OnInit {
+	@ViewChild('fileInput') fileInput: ElementRef;
 	@Output() submited = new EventEmitter();
 	title?: string;
 	editForm: FormGroup;
 	model: Vendor;
+	photo: any = '';
 
 	constructor(
 		public _modalRef: NzModalRef,
 		private _fb: FormBuilder,
 		private _dataService: VendorService,
 		private _notificationService: NzNotificationService,
+		private _modalService: NzModalService,
+		private _cdr: ChangeDetectorRef,
 	) { }
 
 	ngOnInit(): void {
@@ -28,17 +35,57 @@ export class AddVendorModalComponent implements OnInit {
 	}
 
 	initForm() {
+		if (this.model?.photo) {
+			this.photo = 'data:image/png;base64,' + this.model.photo;
+
+		}
 		this.editForm = this._fb.group({
 			id: [this.model?.id ?? 0],
 			vendorCode: [this.model?.vendorCode, Validators.required],
 			vendorName: [this.model?.vendorName, Validators.required],
+			photo: [this.model?.photo]
+		});
+	}
+
+	openFileInput(): void {
+		this.fileInput.nativeElement.click();
+	}
+
+	fileChangeEvent(event: any): void {
+		const cropperModalRef = this._modalService.create({
+			nzContent: LogoCropperComponent,
+			nzClosable: false,
+			nzFooter: null,
+			nzWidth: 700,
+			nzComponentParams: {
+				imageChangedEvent: event
+			},
+		});
+
+		cropperModalRef.afterClose.subscribe(result => {
+			if (result) {
+				this.photo = result;
+				this._cdr.detectChanges();
+			}
+		});
+
+		this._modalService.afterAllClose.subscribe(() => {
+			this.fileInput.nativeElement.value = '';
 		});
 	}
 
 	onSubmit() {
+		var formData: any = new FormData();
+		formData.append('vendorCode', this.editForm.value['vendorCode']);
+		formData.append('vendorName', this.editForm.value['vendorName']);
+		formData.append('photoFile', this.photo ?? base64ToFile(this.photo));
+		// formData.append('photoFile', base64ToFile(this.photo));
+
+
 		this._modalRef.close();
 		if (!this.model) {
-			this._dataService.create(this.editForm.value as Vendor).subscribe({
+			formData.append("id", 0);
+			this._dataService.create(formData).subscribe({
 				next: res => {
 					// this._dataService.list = res as Vendor[];
 					this.submited.emit();
@@ -59,7 +106,8 @@ export class AddVendorModalComponent implements OnInit {
 				}
 			});
 		} else {
-			this._dataService.update(this.editForm.value as Vendor).subscribe({
+			formData.append("id", this.model.id);
+			this._dataService.update(formData).subscribe({
 				next: res => {
 					// this._dataService.list = res as Vendor[];
 					this.submited.emit();
