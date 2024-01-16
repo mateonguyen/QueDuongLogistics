77f317429a5ec274db9a8a0cee4918b5a7a1f0ce
PhucnnPC <phucnn.abnet@gmail.com>
2024-01-16 16:52:23 +0700

    Excel with template

--- frontend/src/app/logistic/transaction/transaction.component.ts
+++ frontend/src/app/logistic/transaction/transaction.component.ts
@@ -9,7 +9,7 @@ import { Vendor } from 'src/app/__models/vendor';
 import { CustomerService } from 'src/app/__services/customer.service';
 import { TransactionService } from 'src/app/__services/transaction.service';
 import { VendorService } from 'src/app/__services/vendor.service';
-import * as XLSX from 'xlsx';
+import { ExportService } from 'src/app/__services/export.service';
 
 
 @Component({
@@ -46,6 +46,7 @@ export class TransactionComponent implements OnInit {
 		private _customerService: CustomerService,
 		private _vendorService: VendorService,
 		@Inject(LOCALE_ID) public locale: string,
+		private exportService: ExportService
 	) {
 		this.term = '';
 		this.sortField = 'Id';
@@ -99,68 +100,11 @@ export class TransactionComponent implements OnInit {
 		let _transDateFrom = this.transDateFrom == null ? null : formatDate(this.transDateFrom, 'yyyyMMdd', this.locale);
 		let _transDateTo = this.transDateTo == null ? null : formatDate(this.transDateTo, 'yyyyMMdd', this.locale);
 
-		this._transactionService.list(this.pageIndex, this.pageSize, this.sortField, this.sortOrder, _transDateFrom, _transDateTo, this.term, this.customerFilter, this.vendorFilter).subscribe(res => {
-			const fileName = 'Xuất_excel_vận_đơn';
-			const data = res.result;
-
-			// Define a mapping between database column names and desired titles
-			const columnTitleMap = {
-				'id' : 'Id', 
-				'transactionNo' : 'Mã điều vận', 
-				'transactionDate' : 'Ngày điều vận',  
-				'customerId' : 'Mã khách hàng',  
-				'customerName' : 'Tên khách hàng',   
-				'customer' : 'Customer', 
-				'vehicleId' : 'Id phương tiện', 
-				'vehicle' : 'Phương tiện', 
-				'driverId' : 'Mã tài xế', 
-				'driver' : 'Driver', 
-				'origin' : 'Điểm giao', 
-				'destination' : 'Điểm nhận', 
-				'vendorId' : 'Mã đơn vị vận chuyển', 
-				'vendor' : 'Vendor', 
-				'vendorName' : 'Tên đơn vị vận chuyển', 
-				'transactionDetails' : 'TransactionDetails'
-			};
-
-
-			// Load the template file
-			this.loadTemplate().then((ws: XLSX.WorkSheet) => {
-				// Add data to worksheet starting from the second row
-				XLSX.utils.sheet_add_json(ws, data, { skipHeader: true, origin: 'A2' });
-
-				// Create a workbook and add the worksheet
-				const wb: XLSX.WorkBook = XLSX.utils.book_new();
-				XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-				// Save the workbook as an XLSX file
-				XLSX.writeFile(wb, fileName + '.xlsx');
-			});
+		this._transactionService.listForExport(this.sortField, this.sortOrder, _transDateFrom, _transDateTo, this.term, this.customerFilter, this.vendorFilter).subscribe(res => {
+			const templatePath = 'assets/template_export.xlsx';
+			const exportFileName = 'exported_data.xlsx';
+			const dataToExport = res;
+			this.exportService.exportData(dataToExport, templatePath, exportFileName);
 		});
 	}
-
-	private loadTemplate(): Promise<XLSX.WorkSheet> {
-		return new Promise((resolve, reject) => {
-		  // Replace 'assets/template_export.xlsx' with the actual path to your template file
-		  const templateFilePath = 'assets/template_export.xlsx';
-	  
-		  try {
-			const workbook: XLSX.WorkBook = XLSX.readFile(templateFilePath, { cellDates: true, cellNF: false, cellText: false });
-	  
-			// Assuming there's only one sheet in the template workbook
-			const sheetName = workbook.SheetNames[0];
-			const worksheet: XLSX.WorkSheet | null = workbook.Sheets[sheetName] || null;
-	  
-			if (worksheet !== null) {
-			  resolve(worksheet);
-			} else {
-			  console.error('Error: The worksheet is null.');
-			  reject('The worksheet is null.');
-			}
-		  } catch (error) {
-			console.error('Error loading template:', error);
-			reject(error);
-		  }
-		});
-	  }
 }
